1200f61fd52c5db64346042554b20b240acf53fb
amy null <null@amy.gg>
2023-06-18 17:12:33 -0400

    🐛 fix: 0.2.25: musl-gcc!

--- libe2fs-sys/build-e2fs.sh
+++ libe2fs-sys/build-e2fs.sh
@@ -14,7 +14,7 @@ mkdir -pv build
 # subs
 # yes, two passes are necessary. the first pass generates the subs files, and
 # the second ./configure actually configures for build.
-./configure
+env CC="musl-gcc" ./configure
 make subs
 make lib/ext2fs/ext2_types.h
 
@@ -27,9 +27,11 @@ cd build
 sed -i -e 's/#include "e2_types.h"/#include "ext2_types.h"/' ../lib/ext2fs/ext2fs.h
 sed -i -e 's/#include "e2_bitops.h"/#include "bitops.h"/' ../lib/ext2fs/ext2fs.h
 sed -i -e 's|#include <ext2fs/ext2_types.h>|#ifdef EXT2_FLAT_INCLUDES\n#include "ext2_types.h"\n#else\n#include <ext2fs/ext2_types.h>\n#endif|g' ../lib/ext2fs/crc16.c ../lib/ext2fs/dosio.c ../lib/ext2fs/ext2_fs.h ../lib/ext2fs/ext2_io.h ../lib/ext2fs/ext2fs.h
+# i swear, musl-gcc causes this to break.
+sed -i -e 's|#include "com_err.h"|#include "../et/com_err.h"|g' ../lib/ext2fs/ext2fs.h
 
 # configure! autotools! pain! :D
-env CFLAGS="-DEXT2_FLAT_INCLUDES=1" ../configure
+env CC="musl-gcc" CFLAGS="-DEXT2_FLAT_INCLUDES=1" ../configure
 
 # patch makefile to only build libext2fs
 perl -i -pe 's/LIB_SUBDIRS=.*\n.*$/LIB_SUBDIRS=lib\/et \$\(EXT2FS_LIB_SUBDIR\) #/igs' Makefile
@@ -40,4 +42,4 @@ cp ../util/subst* ./util/
 
 # do the meme!
 make subs
-env CFLAGS="-DEXT2_FLAT_INCLUDES=1" LDFLAGS="-Wl,static" make libs
+env CC="musl-gcc" CFLAGS="-DEXT2_FLAT_INCLUDES=1" LDFLAGS="-Wl,static" make libs
